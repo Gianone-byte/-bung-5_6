ad659a20ee9925c8a0ae032712d20027f26d5d0b
Gianpiero Dell'Aquila <delgia01@students.zhaw.ch>
2025-11-09 23:16:21 +0100

    fix: MongoDB loading movies correctly

--- src/lib/db.server.js
+++ src/lib/db.server.js
@@ -1,113 +1,18 @@
 
 import { MongoClient, ObjectId } from "mongodb";
 
-const DB_URI = process.env.DB_URI ?? process.env.MONGODB_URI;
-
+const DB_URI = process.env.DB_URI;
 if (!DB_URI) {
-  throw new Error("DB_URI ist nicht gesetzt (Netlify Env Variable fehlt)");
+  throw new Error("DB_URI ist nicht gesetzt (Netlify Env oder .env prüfen)");
 }
 
-
-let clientPromise;
-if (!clientPromise) {
-  const client = new MongoClient(DB_URI);
-  clientPromise = client.connect();
-}
+const client = new MongoClient(DB_URI);
+const clientPromise = client.connect();
 
 export async function getDB() {
   const client = await clientPromise;
-  return client.db("ScreenStackDB"); 
+ 
+  return client.db("ScreenStackDB");
 }
 
-
-
-
-async function getMovies() {
-  try {
-    const db = await getDB();
-    const collection = db.collection("movies");
-    const movies = await collection.find({}).toArray();
-
-  
-    return movies.map((m) => ({
-      ...m,
-      _id: m._id.toString()
-    }));
-  } catch (error) {
-    console.log("getMovies error:", error);
-    return [];
-  }
-}
-
-
-async function getMovie(id) {
-  try {
-    const db = await getDB();
-    const collection = db.collection("movies");
-    const movie = await collection.findOne({ _id: new ObjectId(id) });
-    return movie ? { ...movie, _id: movie._id.toString() } : null;
-  } catch (error) {
-    console.log("getMovie error:", error);
-    return null;
-  }
-}
-
-
-async function createMovie(movie) {
-  
-  movie.poster = movie.poster ?? "/images/placeholder.jpg";
-  movie.actors = movie.actors ?? [];
-  movie.watchlist = movie.watchlist ?? false;
-
-  try {
-    const db = await getDB();
-    const collection = db.collection("movies");
-    const result = await collection.insertOne(movie);
-    return result.insertedId.toString();
-  } catch (error) {
-    console.log("createMovie error:", error);
-    return null;
-  }
-}
-
-
-async function updateMovie(movie) {
-  try {
-    const id = movie._id;
-    delete movie._id;
-
-    const db = await getDB();
-    const collection = db.collection("movies");
-    const result = await collection.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: movie }
-    );
-
-    return result.matchedCount ? id : null;
-  } catch (error) {
-    console.log("updateMovie error:", error);
-    return null;
-  }
-}
-
-
-async function deleteMovie(id) {
-  try {
-    const db = await getDB();
-    const collection = db.collection("movies");
-    const result = await collection.deleteOne({ _id: new ObjectId(id) });
-    return result.deletedCount ? id : null;
-  } catch (error) {
-    console.log("deleteMovie error:", error);
-    return null;
-  }
-}
-
-export default {
-  getMovies,
-  getMovie,
-  createMovie,
-  updateMovie,
-  deleteMovie
-};
 export { ObjectId };
